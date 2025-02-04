6896d372d85061ed810c9cb3dd54fe6521f5091b
Jaroslavs Frolovs <A240132JF@rvt.lv>
2025-02-04 12:53:52 +0200

    mans pirmais commits, datorspēles 'Uzmini skaitļi' sākuma komandas.

---
+++ main.py
@@ -0,0 +1,6 @@
+# Skaitļa minēšanas spēle
+# Izveidoja: Jaroslavs Frolovs
+
+from random import randint
+print("Datorspēle \"Uzmini skaitli\".")
+print('Dators "Izdomajas" veselo skaitli no 1 līdz 10.') 
