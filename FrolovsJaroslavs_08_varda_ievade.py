c7ccb5422f7ff5c878d26ebaae51f1745971ec9e
Jaroslavs Frolovs <A240132JF@rvt.lv>
2025-02-25 11:11:51 +0200

    [DarbsKlasē25/02/2025]

---
+++ FrolovsJaroslavs_08_varda_ievade.py
@@ -0,0 +1,4 @@
+vards = input('Kā tevi sauc?')
+print('Labdien,' , vards)
+teksts = input('Ievadiet pirmo skaitli:')
+skaitlis = int(teksts)
---
+++ FrolovsJaroslavs_09_skaitlu_ievade.py
@@ -0,0 +1,3 @@
+PirmaisSkaitlis = float(input('Ievadiet pirmo skaitļi: '))
+OtraisSkaitlis = float(input('Ievadiet otro skaitļi: '))
+print(PirmaisSkaitlis + OtraisSkaitlis)
