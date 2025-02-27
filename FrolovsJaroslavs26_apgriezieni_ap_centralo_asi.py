39981c866fd390f4371e49110ba46ce3c6cd122b
Jaroslavs Frolovs <A240132JF@rvt.lv>
2025-02-27 08:56:39 +0200

    Izvadītas 4 grupas no 8, bez animacijas.

---
+++ FrolovsJaroslavs26_apgriezieni_ap_centralo_asi.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+   ASCII Animācija. Burtu rotācija ap centralo asi.
+"""
+print("R")
+print(" O")
+print("   T")
+
+print(" R")
+roint(" O")
+print(" T")
+
+print("  R")
+print(" O")
+print("T")
+
+print("T O R")
+
